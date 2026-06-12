9d42536a542cf5cf4a15d66193311e14f1418c9e
Ayush Gupta <ashzex0@gmail.com>
2026-06-13 02:59:52 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,34 +1,34 @@
 import axios from 'axios';
-import config from "../config";
 
-const BASE = config.API_BASE_URL;
+const API_BASE = 'https://crime-analytics.onrender.com';
+const ML_BASE = 'https://python-part-teic.onrender.com';
 
 export const getCrimes = (params) =>
-  axios.get(`${BASE}/api/crimes`, { params });
+  axios.get(`${API_BASE}/api/crimes`, { params });
 
 export const getSummary = () =>
-  axios.get(`${BASE}/api/summary`);
+  axios.get(`${API_BASE}/api/summary`);
 
 export const getStates = () =>
-  axios.get(`${BASE}/api/states`);
+  axios.get(`${API_BASE}/api/states`);
 
 export const getYears = () =>
-  axios.get(`${BASE}/api/years`);
+  axios.get(`${API_BASE}/api/years`);
 
 export const getTrends = (params) =>
-  axios.get(`${BASE}/api/trends`, { params });
+  axios.get(`${API_BASE}/api/trends`, { params });
 
 export const getCrimeTypes = () =>
-  axios.get(`${BASE}/api/crime-types`);
-
-export const getForecast = (params) =>
-  axios.get(`${config.ML_BASE_URL}/forecast`, { params });
-
-export const getClusters = () =>
-  axios.get(`${config.ML_BASE_URL}/clusters`);
+  axios.get(`${API_BASE}/api/crime-types`);
 
 export const getTopStates = (params) =>
-  axios.get(`${BASE}/api/top-states`, { params });
+  axios.get(`${API_BASE}/api/top-states`, { params });
 
 export const getCrimeTypeStats = (params) =>
-  axios.get(`${BASE}/api/crime-stats`, { params });
+  axios.get(`${API_BASE}/api/crime-stats`, { params });
+
+export const getForecast = (params) =>
+  axios.get(`${ML_BASE}/forecast`, { params });
+
+export const getClusters = () =>
+  axios.get(`${ML_BASE}/clusters`);
